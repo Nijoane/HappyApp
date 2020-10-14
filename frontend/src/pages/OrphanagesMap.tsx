e3de9504d819d33afcef2e7e8760c31c2665b419
Nijoane <nicajoane@outlook.com>
2020-10-14 20:12:48 -0300

    Adicionando ao front-end pagina de cadastro e página de contato

--- frontend/src/pages/OrphanagesMap.tsx
+++ frontend/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowDownRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
@@ -9,39 +10,59 @@ import mapMarkerImg from '../images/happy-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
 
+const mapIcon = Leaflet.icon({
+	iconUrl: mapMarkerImg,
+	iconSize:[48, 58],
+	iconAnchor: [24, 58],
+	popupAnchor: [170, 2]
+});
+
 function OrphanagesMap() {
-    return(
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="Happy Logo"/>
+  return(
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy Logo"/>
 
-                    <h2>Escolha um orfanato no mapa</h2>
-                    <p>Muitas crianças estão esperando a sua visita :)</p>
-                </header>
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
 
-                <footer>
-                    <strong>São Paulo</strong>
-                    <span>São Paulo</span>
-                </footer>
-            </aside>
+        <footer>
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
 
-            <Map
-                center={[-23.5699717,-46.6501324]}
-                zoom={15}
-                style={{ width: '100%', height:'100%' }}
-            >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                {/* <TileLayer 
-                    url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
-                /> */}
-            </Map>
+      <Map
+        center={[-23.5699717,-46.6501324]}
+        zoom={15}
+        style={{ width: '100%', height:'100%' }}
+      >
+				<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+				{/* <TileLayer 
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+        /> */}
 
-            <Link to="" className="create-orphanage">
-                <FiPlus size={32} color="#FFF" />
-            </Link>
-        </div>
-    )
+				<Marker 
+					icon={mapIcon}
+					position={[-23.5699717,-46.6501324]}
+				>
+					<Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+						Lar Batista de Crianças de Aclimação
+
+						<Link to="">
+							<FiArrowDownRight size={20} color="#FFF"/>
+						</Link>
+					</Popup>
+				</Marker>
+      </Map>
+
+      <Link to="orphanages/create" className="create-orphanage">
+      	<FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  )
 }
 
 export default OrphanagesMap;
